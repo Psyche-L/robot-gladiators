4267af6d97442860435daab579049598630b2007
Seveonno Palizzolo <homedome@Seveonnos-Air.attlocal.net>
2022-02-08 15:54:20 -0800

    alerts and health indicators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,18 @@
 var playerName = window.prompt("what is your robot's name");
 var playerHealth = 100;
+
+//check if the value of the playerHealth variable is greater than 0
+if (playerHealth > 0) {
+  console.log("Your player is still alive!");
+}
 var playerAttack = 10;
+
+if (playerHealth === 0){
+  console.log("This will not run.");
+}
+else {
+  console.log("This will run instead.");
+}
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -20,12 +32,28 @@ var fight = function() {
   console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
   );
+
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has dies! ");
+  }
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
   playerHealth = playerHealth - enemyAttack
   // Log a resulting message to the console so we know that it worked.
   console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
   );
+
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  }
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
 };
 // execute function
 fight();
